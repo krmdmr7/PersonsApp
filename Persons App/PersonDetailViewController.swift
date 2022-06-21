d8bd2b127f872fa6f293eaad5b4dd6170dd4e238
krmdmr7 <keremdemir1694@gmail.com>
2022-06-21 03:15:13 +0300

    Project completeded.

--- Persons App/PersonDetailViewController.swift
+++ Persons App/PersonDetailViewController.swift
@@ -12,9 +12,17 @@ class PersonDetailViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var phoneNumberLabel: UILabel!
     
+    
+    
+    var kisi:Persons?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        if let k = kisi {
+            nameLabel.text = k.kisi_ad
+            phoneNumberLabel.text = k.kisi_tel
+        }
         
     }
 }
